d3ca415e2b60df1e75c6ecd3aeea8f97d3b58bd4
Raven Hrafnagud <Hrafnfreistudrr@gmail.com>
2021-05-20 12:42:50 -0500

    proyecto terminado hasta la clase del 20 de mayo del 2021

--- src/routers/DashboardRoutes.js
+++ src/routers/DashboardRoutes.js
@@ -4,6 +4,7 @@ import DcScreen from '../components/dc/DcScreen'
 import HeroesScreen from '../components/heroes/HeroesScreen'
 import MarvelScreen from '../components/marvel/MarvelScreen'
 import { Navbar } from '../components/iu/NavBar'
+import SearchScreen from '../components/search/SearchScreen'
 
 function DashboardRoutes() {
     return (
@@ -13,7 +14,8 @@ function DashboardRoutes() {
                 <Switch>
                     <Route exact path="/marvel" component={MarvelScreen}/>
                     <Route exact path="/dc" component={DcScreen}/>
-                    <Route exact path="/hero/_heroId" component={HeroesScreen}/>
+                    <Route exact path="/hero/:heroId" component={HeroesScreen}/>
+                    <Route exact path="/search" component={SearchScreen}/>
                     <Redirect to="/marvel"/>
                 </Switch>
             </div>
